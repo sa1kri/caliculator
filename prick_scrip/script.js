98fc8ca70176b498be10505bf2b332960af630ae
sa1kri <92201506+sa1kri@users.noreply.github.com>
2022-04-09 20:05:46 +0500

    Add files via upload

--- prick_scrip/script.js
+++ prick_scrip/script.js
@@ -1,72 +1,63 @@
 let calc = {
-    numbA: '',
+    numbA:  '',
     act: null,
     numbB: '',
-} 
+}
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
+const numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
-const a
+const actions = ['plus','mine','divi','mult'];
 
 $(document).ready(function () {
+    console.log(calc);
     $(".vvd").text(0);
 
     for (let i = 0; i < numbers.length; i++) {
         const e = numbers[i];
-        
+
+        $("#" + e).click(function() {
+            console.log("#" + e);
+    
+            if(!calc.act) {
+                calc.numbA += e;
+                $(".vvd").text(calc.numbA);}
+            else {
+                calc.numbB += e;
+                $(".vvd").text(calc.numbB);
+            }
+    
+            console.log(calc);
+        });
     }
 
-    $("#" + e ).click(function() {
-        console.log("#" + e);
-
-        if (calc.act == null) {
-            calc.numbA += 'e';
-            $(".vvd").text(calc.numbA);       
-        } else {
-            calc.numbB += 'e';
-            $(".vvd").text(calc.numbB);
-        }
-
-        console.log(calc);
-    });
-
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+    for (let i = 0; i < actions.length; i++) {
+        const e = actions[i];
+        $("#" + e).click(function () {
+            console.log("#" + e);
+    
+            calc.act = e;
+    
+            console.log(calc);
+        });
     }
-
-    $("#plus").click(function(){
-        console.log("#" + e);
-
-        calc.act = e;
-
-        console.log(calc);        
-    });
-
-
-
-    $("#rvn").click(function(){
+    $("#rvn").click(function () {
         console.log("#rvn");
-
-        if (calc.act === 'plus') {
-            rvn = Number(calc.numbA) + (calc.numbB) ; 
+        let rvn = null
+        if(calc.act === 'plus') {
+            rvn = Number(calc.numbA) + Number(calc.numbB);
         }
-
-        if (calc.act === 'mine') {
-            rvn = Number(calc.numbA) - (calc.numbB) ; 
+        if(calc.act === 'mine') {
+            rvn = Number(calc.numbA) - Number(calc.numbB);
         }
-
-        if (calc.act === 'divil') {
-            rvn = Number(calc.numbA) / (calc.numbB) ; 
+        if(calc.act === 'divi') {
+            rvn = Number(calc.numbA) / Number(calc.numbB);
         }
-
-        if (calc.act === 'mult') {
-            rvn = Number(calc.numbA) * (calc.numbB) ; 
+        if(calc.act === 'mult') {
+            rvn = Number(calc.numbA) * Number(calc.numbB);
         }
         $(".vvd").text(rvn);
         calc.numbA = '';
         calc.numbB = '';
         calc.act = null;
-
     });
 });
